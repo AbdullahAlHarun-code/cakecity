af5afe19939dd6366d8c04544a7efb44893399a7
AbdullahAlHarun-code <aharun46@gmail.com>
2020-11-30 23:22:02 +0000

    change settings STATIC_URL

--- cakecity/urls.py
+++ cakecity/urls.py
@@ -34,3 +34,4 @@ urlpatterns = [
 handler404 = 'web.views.error_404_view'
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
