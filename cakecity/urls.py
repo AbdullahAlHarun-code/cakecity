e579099c2b214c4f2835c20d6ade81441c9cd65b
AbdullahAlHarun-code <aharun46@gmail.com>
2020-12-01 11:29:28 +0000

    load media

--- cakecity/urls.py
+++ cakecity/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.media import media
 #from products import views as product_view_new
 urlpatterns = [
     path('admin/', admin.site.urls),
