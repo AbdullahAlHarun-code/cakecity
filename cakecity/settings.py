65f0d22346c2621e69076ddde4285fbe9c4dc3cd
AbdullahAlHarun-code <aharun46@gmail.com>
2020-12-01 01:26:32 +0000

    add cache controll

--- cakecity/settings.py
+++ cakecity/settings.py
@@ -173,9 +173,24 @@ STRIPE_CURRENCY = 'eur'
 
 # aws SetUP
 if 'USE_AWS' in os.environ:
+    #Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Wed, 30 Dec 2080 20:00:00 GMT',
+        'CacheControl': 'max-age-94608000',
+    }
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'cakecity-bucket'
     AWS_S3_REGION_NAME = 'EU (Ireland) eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'csutom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'csutom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Ovverride staic and media urls in production
+    STAIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
