94ad1dee01f4dbba05613853e45be2ee4d24346e
AbdullahAlHarun-code <aharun46@gmail.com>
2020-12-02 02:29:03 +0000

    still modified settings.py for staticfile

--- cakecity/settings.py
+++ cakecity/settings.py
@@ -216,6 +216,21 @@ if 'USE_AWS' in os.environ:
     # Ovverride staic and media urls in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIAFILES_DIRS = (os.path.join(BASE_DIR, 'media'),)
+
+
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 # heroku config:set DEBUG_COLLECTSTATIC=1
 # heroku config:set DISABLE_COLLECTSTATIC=1
